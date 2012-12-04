6c3ab1d03496ac5039eb0174968deb25795a33a8
fizzyD <scott.l.dyer@gmail.com>
2012-12-04 15:01:01 -0500

    improved DishModel so that it is initialized at the start of a generation. Closer to having the ability to run multiple generations and evolve each.

--- src/games/cellz/DishController.java
+++ src/games/cellz/DishController.java
@@ -9,6 +9,8 @@ import java.awt.event.WindowEvent;
 import java.awt.*;
 import java.util.Random;
 
+import org.neuroph.contrib.neat.gen.*;
+
 public class DishController extends Thread {
     DishModel model;
     DishView view;
@@ -25,6 +27,16 @@ public class DishController extends Thread {
     }
 
     public DishController(int nFood) {
+
+    	this.DishInit(nFood);
+        start();
+    }
+    
+    public void DishInit(int nFood) {
+    	if (frame != null){
+        	frame.dispose();
+    	}
+    	//frame.removeAll(); // clear the frame
         frame = new JFrame();
         frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
@@ -34,7 +46,7 @@ public class DishController extends Thread {
         frame.setTitle("Cellz!");
         // get same behaviour every time for now
         model = new DishModel(width, height, new Random(1) );
-        CellModel cell = new CellModel(width / 2, height / 2, new GreedyControl());
+        CellModel cell = new CellModel(width / 2, height / 2, new BruteControl());
         model.add(cell);
         // Food food = new Food(width * 2 / 3, height * 2 / 3, 5);
         model.addRandomFood( nFood );
@@ -45,44 +57,57 @@ public class DishController extends Thread {
         frame.show();
         label = new Label("################################");
         // new CloseableFrame(label, "Timing", true);
-        start();
     }
 
     public void run() {
     	int update_to_display_ratio = 100;
-    	StatisticalSummary mt = new StatisticalSummary();
         ElapsedTimer t = new ElapsedTimer();
-        int x = 0;
-        view.repaint();
-        mt.add( t.elapsed());
-        System.out.println( mt );
-        final int iterations = 10000;
-        // for some number of simulations
-        // run a number of different Cells
-        // then apply genetics to evolve them
-        int iteration = 0;
-        while (iterations > iteration) {
-        	++iteration;
-            try {
-                model.updateModel();
-                if ((0 == iteration%update_to_display_ratio)) {
-                    // label.setText( model.nParticles() + " : " + ( x * 1000 ) / t.elapsed() );
-                    // mt.add( t.elapsed() );
-                    // t.reset();
-                    // mt.reset();
-                	mt.add( t.elapsed());
-                    System.out.println( mt );
-                    System.out.println("iteration = " + iteration);
-                    System.out.println( "cell population " +model.nParticles() );                    
-                	//System.out.println("repaint");
-                	view.repaint();
-                	sleep(draw_delay);
-                }
-            } catch (Exception e) {
-                // System.out.println( e );
-                e.printStackTrace();
-            }
+    	final int iterations = 5000;
+        final int generations = 10;
+        int current_generation = 0;
+        while(generations > current_generation){
+        	int x = 0;
+        	StatisticalSummary mt = new StatisticalSummary();
+        	DishInit(10);
+        	try {
+        		view.repaint();
+        		sleep(draw_delay);
+        	} catch (Exception e){
+        		e.printStackTrace();
+        	}
+        	mt.add( t.elapsed());
+        	System.out.println( mt );
+        	// for some number of simulations
+        	// run a number of different Cells
+        	// then apply genetics to evolve them
+        	int iteration = 0;
+        	while (iterations > iteration) {
+        		++iteration;
+        		try {
+        			model.updateModel();
+        			if ((0 == iteration%update_to_display_ratio)) {
+        				// label.setText( model.nParticles() + " : " + ( x * 1000 ) / t.elapsed() );
+        				// mt.add( t.elapsed() );
+        				// t.reset();
+        				// mt.reset();
+        				mt.add( t.elapsed());
+        				System.out.println( mt );
+        				System.out.println("iteration = " + iteration);
+        				System.out.println( "cell population " +model.nParticles() );                    
+        				//System.out.println("repaint");
+        				view.repaint();
+        				sleep(draw_delay);
+        			}
+        		} catch (Exception e) {
+        			// System.out.println( e );
+        			e.printStackTrace();
+        		}
+        	}
+        	
+        	System.out.println("Generation = "+current_generation);
+        	++current_generation;
         }
+        
         //System.out.println( "particles " +model.nParticles() );
     }
 }
